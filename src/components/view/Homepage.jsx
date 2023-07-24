a5f00ccf4e242eeb798d4b83abd064ef35d17142
gunjan <gunjandora595@gmail.com>
2023-07-24 13:52:06 +0530

    4 section done

--- src/components/view/Homepage.jsx
+++ src/components/view/Homepage.jsx
@@ -3,15 +3,19 @@ import OurVision from "../homepage/OurVision";
 import Mission from "../homepage/Mission";
 import OurStory from "../homepage/OurStory";
 import OurTeam from "../homepage/OurTeam";
+import BackToTop from "../common/BackToTop";
 
 const Homepage = () => {
   return (
-    <div className="overflow-hidden">
-      <OurVision />
-      <Mission />
-      <OurStory />
-      <OurTeam />
-    </div>
+    <>
+      <div className="overflow-hidden">
+        <OurVision />
+        <Mission />
+        <OurStory />
+        <OurTeam />
+        <BackToTop />
+      </div>
+    </>
   );
 };
 
